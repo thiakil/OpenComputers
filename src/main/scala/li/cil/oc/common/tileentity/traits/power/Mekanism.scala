7d19b63b3ffeab312efa1720285359d87694b738
Xander Victory <x@xandervictory.id.au>
2018-05-08 17:27:37 +0800

    re-add Mekanism power support

--- src/main/scala/li/cil/oc/common/tileentity/traits/power/Mekanism.scala
+++ src/main/scala/li/cil/oc/common/tileentity/traits/power/Mekanism.scala
@@ -1,29 +1,30 @@
 package li.cil.oc.common.tileentity.traits.power
-/* TODO Mekanism
 
-import cpw.mods.fml.common.Optional
 import li.cil.oc.common.asm.Injectable
 import li.cil.oc.integration.Mods
 import li.cil.oc.integration.util.Power
-import net.minecraftforge.common.util.ForgeDirection
+import net.minecraft.util.EnumFacing
+import net.minecraftforge.fml.common.Optional
 
-@Injectable.Interface(value = "mekanism.api.energy.IStrictEnergyAcceptor", modid = Mods.IDs.Mekanism)
+@Injectable.InterfaceList(Array(
+  new Injectable.Interface(value = "mekanism.api.energy.IStrictEnergyAcceptor", modid = Mods.IDs.Mekanism),
+  new Injectable.Interface(value = "mekanism.api.energy.IStrictEnergyStorage", modid = Mods.IDs.Mekanism)
+))
 trait Mekanism extends Common {
   @Optional.Method(modid = Mods.IDs.Mekanism)
-  def canReceiveEnergy(side: ForgeDirection) = Mods.Mekanism.isAvailable && canConnectPower(side)
+  def canReceiveEnergy(side: EnumFacing):Boolean = Mods.Mekanism.isModAvailable && canConnectPower(side)
 
   @Optional.Method(modid = Mods.IDs.Mekanism)
-  def transferEnergyToAcceptor(side: ForgeDirection, amount: Double) =
-    if (!Mods.Mekanism.isAvailable) 0
-    else Power.toJoules(tryChangeBuffer(side, Power.fromJoules(amount)))
+  def acceptEnergy(side: EnumFacing, amount: Double, simulate: Boolean): Double =
+    if (!Mods.Mekanism.isModAvailable) 0D
+    else Power.toJoules(tryChangeBuffer(side, Power.fromJoules(amount), !simulate))
 
   @Optional.Method(modid = Mods.IDs.Mekanism)
-  def getMaxEnergy = Power.toJoules(ForgeDirection.VALID_DIRECTIONS.map(globalBufferSize).max)
+  def getMaxEnergy: Double = Power.toJoules(EnumFacing.values.map(globalBufferSize).max)
 
   @Optional.Method(modid = Mods.IDs.Mekanism)
-  def getEnergy = Power.toJoules(ForgeDirection.VALID_DIRECTIONS.map(globalBuffer).max)
+  def getEnergy: Double = Power.toJoules(EnumFacing.values.map(globalBuffer).max)
 
   @Optional.Method(modid = Mods.IDs.Mekanism)
-  def setEnergy(energy: Double) {}
+  def setEnergy(energy: Double) = Unit
 }
-*/
